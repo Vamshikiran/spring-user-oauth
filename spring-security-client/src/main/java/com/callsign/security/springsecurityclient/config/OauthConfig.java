a39033020364db29a4bbe0d71594d957adecfc87
vamshinoetic <vamshi.gullapelly@wearenoetic.com>
2018-11-14 15:16:59 +0000

    Initial commit

---
+++ spring-security-client/src/main/java/com/callsign/security/springsecurityclient/config/OauthConfig.java
@@ -0,0 +1,24 @@
+package com.callsign.security.springsecurityclient.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@EnableOAuth2Client
+@Configuration
+public class OauthConfig extends WebSecurityConfigurerAdapter{
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+    }
+}
